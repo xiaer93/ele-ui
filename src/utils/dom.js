36492b3067060bcf2deb2257631cba102a848722
chengjw <chengjw@51cto.com>
2019-10-11 19:37:16 +0800

    添加滚动组件，不依赖原生如何实现？

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,3 +1,32 @@
+// 立即执行函数，本质上是一种优化。JS高性能。
 export const on = (function () {
-  
+  if (document.addEventListener) {
+    return function (element, event, handler) {
+      if (element && event && handler) {
+        element.addEventListener(event, handler, false)
+      }
+    }
+  } else {
+    return function (element, event, handler) {
+      if (element && event && handler) {
+        element.attachEvent('on' + event, handler)
+      }
+    }
+  }
+})()
+
+export const off = (function () {
+  if (document.removeEventListener) {
+    return function (element, event, handler) {
+      if (element && event) {
+        element.removeEventListener(event, handler, false)
+      }
+    }
+  } else {
+    return function (element, event, handler) {
+      if (element && event) {
+        element.detachEvent('on' + event, handler)
+      }
+    }
+  }
 })()
